f129cacc51b64b60558ccd074a88c17fb2f3d930
Emiliano <cararoemilianofrontend@gmail.com>
2025-10-27 19:05:11 -0300

    cambioImg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TitanoMaquiaGames",
+  title: "TitanomaquiaGames",
   description: "Desarrolladora de videojuegos",
 };
 
